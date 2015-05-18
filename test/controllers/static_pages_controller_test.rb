3901b93912f74910a137d8ac36cea0ffc6fd1425
alfonsbauer <alfonsbauer102@gmail.com>
2015-05-18 07:43:49 -0700

    Layouts & Embedded

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,16 +7,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should got home" do
   	get :home
   	assert_response :success
+  	assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should got help" do
   	get :help
   	assert_response :success
+  	assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should got about" do
   	get :about
   	assert_response :success
+  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
